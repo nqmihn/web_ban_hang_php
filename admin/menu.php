c3bb71d3de574baeb1f8aea1c0f0bb0d0c285ada
GraySora16 <qminhqb@gmail.com>
2022-11-19 10:46:47 +0700

    Nha san xuat

---
+++ admin/menu.php
@@ -0,0 +1,15 @@
+<ul>
+    <li>
+        <a href="../manufacturers">
+                Quản lý nhà sản xuất
+        </a>
+    </li>
+    <li>
+        <a href="../products">
+                Quản lý sản phẩm
+        </a>
+    </li>
+    <li>
+        <a href="../orders">Quản lý đơn hàng</a>
+    </li>
+</ul>
