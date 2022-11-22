02bc054cc2c2dc1571b9617b93e154c0bb2c0e0c
Gray16 <96378890+nqmihn@users.noreply.github.com>
2022-11-22 21:01:49 +0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -21,8 +21,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'qminhqb3@gmail.com';                     //SMTP username
-    $mail->Password   = 'jdjkfqdfuijjuxcj';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->CharSet = 'UTF-8';
